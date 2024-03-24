de47e99008dfdbe1f7a35ccf04f67a1176960213
tuyetnk <tuyetnk@inter-k.com>
2024-03-24 13:14:43 +0700

    ktiet: upload Course Seletor trong Select Items

--- skillboost_react_php/src/App.jsx
+++ skillboost_react_php/src/App.jsx
@@ -1,10 +1,12 @@
 //import {} from './components'
+import CourseSelector from './components/ui/SelectItems/CourseSelector';
 import './font.css'
 import TestComponents from './pages/TestComponents'
 
 const App = () => (
   <main className="relative">
-      <TestComponents />
+      <CourseSelector/>
+      <TestComponents/>
   </main>
 )
 
