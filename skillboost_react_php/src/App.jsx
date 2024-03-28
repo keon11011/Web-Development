12a6cc553cd49f4fe6c8c355d9a481353a6a678d
Vĩ Bùi <vibuixuan2812@gmail.com>
2024-03-28 13:53:22 +0700

    Finish select-item (vipui)

--- skillboost_react_php/src/App.jsx
+++ skillboost_react_php/src/App.jsx
@@ -1,20 +1,24 @@
 //import {} from './components'
 import './font.css'
 import TestComponents from './pages/TestComponents'
+import CommentCard from './components/ui/card/CommentCard';
 import SidebarNV from '../src/components/ui/sidebar/SidebarNV'
 import SidebarQL from '../src/components/ui/sidebar/SidebarQL'
 import LeadInfoTab from './components/ui/tabs/LeadInfoTab'
 import Pagnitation from './components/ui/pagnitation/pagnitation';
-// import TestComponents from './pages/TestComponents'
-import TestCard from './pages/TestCard'
 import CourseSelector from './components/ui/SelectItems/CourseSelector';
 import LeadListFilter from './components/ui/SelectItems/LeadListFilter';
+import LeadActivityListFilter from './components/ui/SelectItems/LeadActivityListFilter';
+import CustomerListFilter from './components/ui/SelectItems/CustomerListFilter';
+import DiscountListFilter from './components/ui/SelectItems/DiscountListFilter';
+
+
 
 const App = () => (
   <main className="relative">
-      <CourseSelector/>
-      <LeadListFilter/>
-      <TestComponents/>
+    <LeadActivityListFilter />
+    <CustomerListFilter />
+    <DiscountListFilter/>
   </main>
 )
 
