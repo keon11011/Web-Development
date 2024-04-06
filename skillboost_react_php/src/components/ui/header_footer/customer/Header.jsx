ba0bd6f871d91e28f49c83e313d62a0f166e1c74
Vĩ Bùi <vibuixuan2812@gmail.com>
2024-04-06 09:39:56 +0700

    Finish Resposive TrangChu + Tối ưu header + footer

--- skillboost_react_php/src/components/ui/header_footer/customer/Header.jsx
+++ skillboost_react_php/src/components/ui/header_footer/customer/Header.jsx
@@ -1,44 +1,50 @@
 import SkillboostLogo from '../../../../assets/logo/SkillboostLogo.svg'
 
 import { Routes, Route, Link } from 'react-router-dom'
-import TrangChu from '../../../../pages/TrangChu.jsx'
-
-
+import ButtonFloat from "../../button/ButtonFloat.jsx"
+import HamburgerMd from "../../../icons/Menu/HamburgerMd.jsx"
+import DropDown from "../../placeholder/DropDown.jsx"
 
 function Header() {
   return (
-    <div className='xl:px-40 px-2/5 py-4 text-text-secondary space-x-auto flex w-full justify-between '>
-      <div className='w-[238px] h-[46px] max-w-max min-w-max cursor-pointer'>
-        <Link to="/trangchu">
-          <img
-            src={SkillboostLogo}
-            alt="Logo"
-            width={300}
-            height={100}
-          />
-        </Link>
+    <main>
+      <div className='max-sm:px-5 sm:px-40 px-2/5 py-4 text-text-secondary space-x-auto flex w-full justify-between items-center'>
+        <div className=' flex max-w-max min-w-max cursor-pointer'>
+          <Link to="/trangchu">
+            <img
+              src={SkillboostLogo}
+              alt="Logo"
+            />
+          </Link>
+        </div>
+
+
+        <nav className="title-medium text-text-secondary ">
+          <div name="mobile_devices" className='hidden max-sm:flex cursor-pointer hover:text-brand-default pr-6'>
+            <HamburgerMd>
+            </HamburgerMd>
+          </div>
+          <nav name="desktop" className='max-sm:hidden sm:flex flex-row items-center  gap-12'>
+            <ul className='py-2 px-2 '>
+              <li>
+                <Link to="/trangchu" className="hover:text-brand-default font-bold">Trang chủ</Link>
+              </li>
+            </ul>
+            <ul className=' py-2 px-2 '>
+              <li>
+                <Link to="/khoahoc" className="hover:text-brand-default font-bold">Khóa học</Link>
+              </li>
+            </ul>
+            <ul className=' py-2 px-2 '>
+              <li>
+                <Link to="/dangnhap" className="hover:text-brand-default font-bold"><ButtonFloat>Đăng nhập</ButtonFloat></Link>
+              </li>
+            </ul>
+          </nav>
+        </nav>
       </div>
-      <nav className="flex flex-row title-medium text-text-secondary gap-12">
-        <ul className=' py-2 px-2 '>
-          <li>
-            <Link to="/trangchu" className="hover:text-brand-default font-bold">Trang chủ</Link>
-          </li>
-        </ul>
-        <ul className=' py-2 px-2 '>
-          <li>
-            <Link to="/khoahoc" className="hover:text-brand-default font-bold">Khóa học</Link>
-          </li>
-        </ul>
-        <ul className=' py-2 px-2 '>
-          <li>
-            <Link to="/lienhe" className="hover:text-brand-default font-bold">Liên hệ</Link>
-          </li>
-        </ul>
-      </nav>
+    </main>
 
-
-
-    </div>
 
   )
 }
