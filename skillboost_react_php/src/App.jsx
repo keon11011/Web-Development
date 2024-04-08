12521e7e17cf25c1174aa3a2ce8382139f8b9de6
Vĩ Bùi <vibuixuan2812@gmail.com>
2024-04-08 22:05:36 +0700

    push v2

--- skillboost_react_php/src/App.jsx
+++ skillboost_react_php/src/App.jsx
@@ -1,7 +1,7 @@
 import './font.css'
 import TrangChu from './pages/TrangChu'
 import Dashboard from './pages/Dashboard';
-import BarChart from './components/ui/chart/BarChart';
+
 const App = () => (
   <main className="relative">
     {/* <TrangChu/> */}
